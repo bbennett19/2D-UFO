43e585027cc4242522e77f2598f933a97085960f
Ben <benbennett1991@gmail.com>
2017-04-10 21:48:31 -0700

    Project setup and the addition of a particle system for the pickups that magnitize to the player

---
+++ 2D-UFO-Tutorial/Assets/_Completed-Assets/Scripts/PickupController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    public GameObject pickupParticleSystem;
+
+    public void Start()
+    {
+
+    }
+
+    public void Pickup()
+    {
+        GameObject clone = Instantiate(pickupParticleSystem, this.transform.position, this.transform.rotation);
+        ParticleSystem particleSystem = clone.GetComponent<ParticleSystem>();
+        particleSystem.Play();
+        gameObject.SetActive(false);
+    }
+}
